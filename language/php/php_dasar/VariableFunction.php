fa4a2a5e102e51eae5d8a0ef108121b86cc7f4df
Tonni <49497725+tonni1973@users.noreply.github.com>
2025-01-06 20:57:59 +0700

    Add files via upload

---
+++ language/php/php_dasar/VariableFunction.php
@@ -0,0 +1,17 @@
+<?php
+
+function foo()
+{
+    echo "Hello Foo" . PHP_EOL;
+}
+
+function bar()
+{
+    echo "Hello Bar" . PHP_EOL;
+}
+
+$functionFoo = "foo";
+$functionFoo();
+
+$functionBar = "bar";
+$functionBar();
