0849b674b579b0cc8ebee5511f1e3790a781db2f
Tonni <49497725+tonni1973@users.noreply.github.com>
2025-01-07 06:48:59 +0700

    Add files via upload

---
+++ language/php/php_dasar/StringFunction.php
@@ -0,0 +1,12 @@
+<?php
+
+    // menggabungkan array menjad string
+    // join
+    var_dump(join("-", [1, 2, 3, 4 ,5]));
+
+    // string menjadi array
+    // explode
+    var_dump(explode(" ", "Mirana Davion Marci"));
+
+    // cut substring
+    var_dump(substr("Mirana Davion", 0, 6));
