7bf4f9207bbc778538bb85bddf895ca4a18d115d
Tonni <49497725+tonni1973@users.noreply.github.com>
2025-01-10 20:34:59 +0700

    implementasi namespace

---
+++ language/php/php_oop/data/Conflict.php
@@ -0,0 +1,16 @@
+<?php
+
+// namespace digunakan untuk mengelompokan class, fungsi, interface, konstanta
+
+namespace Data\One {
+    class Conflict {
+
+    }
+}
+
+
+namespace Data\Two {
+    class Conflict {
+
+    }
+}
