b5d65a7d47907a30eb4d711f8817fba51a8a22f9
Tonni <49497725+tonni1973@users.noreply.github.com>
2025-01-21 19:04:07 +0700

    Add files via upload

---
+++ language/php/php_oop/data/Helper.php
@@ -0,0 +1,11 @@
+<?php
+
+// tidak perlu menggunakan kurung kurawal karena satu file ini hanya berisi namespace helper saja
+namespace Helper;
+
+function helpMe()
+{
+    echo "HELP ME" . PHP_EOL;
+}
+
+const APPLICATION = "Belajar PHP OOP";
